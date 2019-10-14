6ec0eaaeff997dc9ea8e335aac5d1ebd23c618af
Andrei <and64region2015@gmail.com>
2019-10-14 12:43:19 +0300

    5 пункт готов есть вопрос по update

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,7 +1,16 @@
 package ru.javawebinar.topjava.service;
 
 import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.util.Collection;
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFound;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
 @Service
 public class MealService {
@@ -12,6 +21,24 @@ public class MealService {
         this.repository = repository;
     }
 
+    public Meal create(Meal meal, int userId) {
+        return repository.save(meal, userId);
+    }
 
+    public void delete(int id, int userId) throws NotFoundException {
+        checkNotFoundWithId(repository.delete(id, userId), id);
+    }
+
+    public Meal get(int id, int userId) throws NotFoundException {
+        return checkNotFoundWithId(repository.get(id, userId), id);
+    }
+
+    public Collection<Meal> getAll(int userId) {
+        return repository.getAll(userId);
+    }
+
+    public void update(Meal meal, int userId) throws NotFoundException {
+        checkNotFoundWithId(repository.save(meal, userId), meal.getId());
+    }
 
 }
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,79 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Collection;
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.MealsUtil.getFilteredTos;
+import static ru.javawebinar.topjava.util.MealsUtil.getTos;
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+
+@Controller
 public class MealRestController {
-    private MealService service;
+    private final MealService service;
+
+    MealRestController(MealService service){
+        this.service = service;
+    }
+
+    public Meal create(Meal meal) throws NotFoundException  {
+        if (mealUserIdEqualsCurrentUserId(meal))
+            throw new NotFoundException("Еда не принадлежит пользователю");
+
+        return service.create(meal, SecurityUtil.authUserId());
+
+    }
+
+    public void update(Meal meal, int userId){
+        if (mealUserIdEqualsCurrentUserId(meal))
+            throw new NotFoundException("Еда не принадлежит пользователю");
+//          Под ВОПРОСОМ
+//        assureIdConsistent(meal, userId);
+
+        service.update(meal, SecurityUtil.authUserId());
+    }
+
+    public Meal get(int id) throws  NotFoundException{
+        Meal meal = service.get(id, SecurityUtil.authUserId());
+
+        if (mealUserIdEqualsCurrentUserId(meal))
+            throw new NotFoundException("Еда не принадлежит пользователю");
+
+        return meal;
+    }
+
+    public void delete(int id) throws NotFoundException {
+        Meal meal = service.get(id, SecurityUtil.authUserId());
+
+        if (mealUserIdEqualsCurrentUserId(meal))
+            throw new NotFoundException("Еда не принадлежит пользователю");
+
+        service.delete(id, SecurityUtil.authUserId());
+    }
+
+    public List<MealTo> getDateTime(LocalTime startTime, LocalTime endTime){
+        return getFilteredTos(service.getAll(SecurityUtil.authUserId()),
+                SecurityUtil.authUserCaloriesPerDay(),
+                startTime,
+                endTime);
+
+
+    }
+
+    public List<MealTo> getAll() {
+        return getTos(service.getAll(SecurityUtil.authUserId()),SecurityUtil.authUserCaloriesPerDay());
+    }
+
+    private boolean mealUserIdEqualsCurrentUserId(Meal meal){
+        return meal.getUserId() != SecurityUtil.authUserId();
+    }
 
 }
