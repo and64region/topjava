9d5a9ae441580422e51c8c730e4655b7dd019ead
Andrei <and64region2015@gmail.com>
2019-10-18 15:57:42 +0300

    сделал запросы в DB

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,8 +1,10 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START WITH 100000;
+
 
 CREATE TABLE users
 (
@@ -21,5 +23,15 @@ CREATE TABLE user_roles
   user_id INTEGER NOT NULL,
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+  id          SERIAL PRIMARY KEY           NOT NULL,
+  dateTime    TIMESTAMP DEFAULT now()       NOT NULL,
+  description VARCHAR                       NOT NULL,
+  calories    INTEGER                       NOT NULL,
+  user_id     INTEGER                       NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -9,3 +9,8 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals(description, calories, user_id) VALUES
+  ('breakfast', 500, 100000),
+  ('lunch', 1000, 100000),
+  ('dinner', 500, 100000);
