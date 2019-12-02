228caf5d791e0b50226286c5e46c024e9d3ecf95
Andrei Artemchuk <and64region2015@gmail.com>
2019-12-02 21:40:21 +0300

    1 ready

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,45 @@
+$(function () {
+    makeEditable({
+        ajaxUrl: "ajax/profile/meals/",
+        datatableApi: $("#datatable").DataTable({
+            "paging": false,
+            "info": true,
+            "columns": [
+                {
+                    "data": "dateTime"
+                },
+                {
+                    "data": "description"
+                },
+                {
+                    "data": "calories"
+                },
+                {
+                    "defaultContent": "Edit",
+                    "orderable": false
+                },
+                {
+                    "defaultContent": "Delete",
+                    "orderable": false
+                }
+            ],
+            "order": [
+                [0,
+                    "asc"
+                ]
+            ]
+
+        }),
+        updateTable: filter
+
+
+    })
+});
+
+function filter() {
+    $.ajax({
+        type: "GET",
+        url: "ajax/profile/meals/filter",
+        data: $("#filter").serialize()
+    }).done(updateTableByData);
+}
